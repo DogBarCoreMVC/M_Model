1b3cdaa12a19978de2acae3ec5a15eb74afc4a48
GitDogBar <Dogbar>
2021-08-16 13:36:48 +0700

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using M_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M_Model.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product product = new Product()
+            {
+                ID = 1,
+                Name = "ASP.NET CORE MVC",
+                Price = 350
+            };
+
+            return View(product);
+        }
+    }
+}
